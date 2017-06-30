a05dde8c21b339a44e879e7d29a61c069821e5f0
Kirill Bespalov <kbespalov@mirantis.com>
2017-06-30 13:31:31 +0300

    add cli entrypoint

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import setup
 setup(name='arbitrage watcher client',
       version='0.1.0',
       packages=['watcher'],
-      install_requires=['requests', 'PyYAML'])
+      install_requires=['requests', 'PyYAML'],
+      entry_points={
+            'console_scripts': [
+                  'arbitrage-watcher = watcher.cli:main'
+            ]
+      })
